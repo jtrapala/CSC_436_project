c5c69734df3d7eac94080c4e2ddab9b25160e827
jtrapala <jane_trapala@my.uri.edu>
2020-02-26 14:10:17 -0500

    Sample code is now running with python3

--- sql_pin_1.py
+++ sql_pin_1.py
@@ -1,10 +1,9 @@
 
 
 '''This is a test for using sqLite 3 with PINCode 
-It creates a database for storing pins and names
-Do not run if already made, I think it will make a blank copy
+It creates a database for donors, recipients, staff, blood types, and ...
 '''
-from flask import Flask, render_template, request
+import flask
 import sqlite3 as lite
 import datetime
 import os
@@ -12,75 +11,88 @@ import os
 from os.path import join, dirname, realpath
 
 
-
 global conn, c
 
+
 def start_c1():
-	conn = lite.connect('pin_dtb.db')
-	return conn
+    conn = lite.connect('pin_dtb.db')
+    return conn
+
 
 def start_c2(conn):
-	c = conn.cursor()
-	return c
-#Creates admin table
+    c = conn.cursor()
+    return c
+# Creates admin table
+
+
 def adt(c):
-	c.execute("""
+    c.execute("""
 		create table admins(
 			user,
 			pw);""")
 
-#Creates user/pin/time table
+# Creates user/pin/time table
+
+
 def upt(c):
-	c.execute("""create table users_pin(
+    c.execute("""create table users_pin(
 		userN,
 		userPin,
 		time);""")
+
+
 def adm_add(nm, users, c, conn):
-	u=(nm,users[nm])
-	c.execute('insert into admins values (?,?)',u)
-	conn.commit()
+    u = (nm, users[nm])
+    c.execute('insert into admins values (?,?)', u)
+    conn.commit()
+
 
 def unk_pin(c, code, conn):
-	unk='unknown'
-	time=str (datetime.datetime.now())
-	u=(unk, code, time)
-	print u
-	c.execute('insert into users_pin values (?,?,?)', u)
-	conn.commit()
+    unk = 'unknown'
+    time = str(datetime.datetime.now())
+    u = (unk, code, time)
+    print(u)
+    c.execute('insert into users_pin values (?,?,?)', u)
+    conn.commit()
+
 
 def kn_pin(c, users, nm, conn):
-	time=datetime.datetime.now()	
-	u=(nm, users[nm], time)
-	c.execute('insert into users_pin values (?,?,?)', u)
-	conn.commit()
+    time = datetime.datetime.now()
+    u = (nm, users[nm], time)
+    c.execute('insert into users_pin values (?,?,?)', u)
+    conn.commit()
 
 
 def db_upr(c):
-	#from PINCode_1 import conn,c
-	#Print USERS
-	print "---------------USERS------------------------" 
-	for row in c.execute('SELECT * FROM admins'):
-		print row
-	print "\n"
+    #from PINCode_1 import conn,c
+    # Print USERS
+    print("---------------USERS------------------------")
+    for row in c.execute('SELECT * FROM admins'):
+        print(row)
+    print("\n")
+
 
 def db_ppr(c):
-	#from PINCode_1 import conn,c
-	#Print PIN_ENTRIES
-	print "---------------PIN_ENTRIES------------------" 
-	for row in c.execute('SELECT * FROM users_pin ORDER BY userN'):
-		print row
+    #from PINCode_1 import conn,c
+    # Print PIN_ENTRIES
+    print("---------------PIN_ENTRIES------------------")
+    for row in c.execute('SELECT * FROM users_pin ORDER BY userN'):
+        print(row)
+
+
 def db_close(conn):
-	#from PINCode_1 import conn,c
-	conn.close
-	print "\nPIN Database is now closed"
-	print "\n#####################################\n"
-	
-	
+    #from PINCode_1 import conn,c
+    conn.close
+    print("\nPIN Database is now closed")
+    print("\n#####################################\n")
+
+
 def dtb_del(conn, c):
-	print "Database clearing"
-	for row in c.execute('delete from users_pin '):
-		print "Deleting row..."
-	
+    print("Database clearing")
+    for row in c.execute('delete from users_pin '):
+        print("Deleting row...")
+
+
 '''	
 UPLOADS_PATH = join(dirname(realpath(__file__)), 'C:/Users/aamim/Documents/GitHub/ele_408_final_project/templates')
 app = Flask(__name__, template_folder=UPLOADS_PATH)
@@ -103,5 +115,3 @@ if __name__ == '__main__':
 
 
 '''
-
-
