5e621c0f13b28083395bb9ac46c996a1fcd9e897
jtrapala <jane_trapala@my.uri.edu>
2020-03-30 20:50:24 -0400

    Separated create and select scripts

---
+++ select-drop-table-bmary.sql
@@ -0,0 +1,24 @@
+
+#Donor table
+select * from pres1.donor;
+drop table donor;
+
+#Staff table
+select * from pres1.staff;
+drop table staff;
+
+# Bloodbag table
+select * from pres1.blood;
+drop table blood;
+
+#Recipients table
+select * from pres1.recipient;
+drop table recipient;
+
+#Blood banks table
+select * from pres1.blood_bank;
+drop table blood_bank;
+
+#Blood drives table
+select * from pres1.blood_drive;
+drop table blood_drive;
