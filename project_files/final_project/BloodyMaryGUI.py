12b868da4fa9b06cfdc37dc7b02a9fcbca1553f0
jtrapala <jane_trapala@my.uri.edu>
2020-04-21 11:32:33 -0400

    Finshed adding functionality to buttons

--- project_files/final_project/BloodyMaryGUI.py
+++ project_files/final_project/BloodyMaryGUI.py
@@ -42,11 +42,12 @@ def buttonHandler(arg1):
     print("Add entry: ", arg1)
     # print number
     if arg1 == "blood_drive":
-        new_entry1 = [int(x)
-                      for x in add_blood_drive_entry.get().split(',')]
-        print(new_entry1)
+        e = add_blood_drive_entry.get()
+        print("Entry:",e)
+        new_entry1=list(e.split(', '))
+        print(len(new_entry1))
         # Checks if the number length is <4, if it is, tell the user
-        if len(new_entry1) != 7:
+        if len(new_entry1) != 8:
             print("Incorrect attribute size")
         else:
             # Add pin entry to record database
@@ -57,7 +58,7 @@ def buttonHandler(arg1):
 
     if arg1 == "donor":
         e = add_donor_entry.get()
-        print(e)
+        print("Entry:",e)
         new_entry2=list(e.split(', '))
         #print(new_entry2)
         # Checks if the number length is <4, if it is, tell the user
@@ -69,11 +70,38 @@ def buttonHandler(arg1):
             db.see_donors(c)
             #records(root,conn)
         # Deletes the number array on an ENTER
-            
-    # If the settings button is pressed, then have username
-    # and password prompt appear
-
-
+            del new_entry2[:]
+    if arg1 == "recipient":
+        e = add_recipient_entry.get()
+        print("Entry:",e)
+        new_entry3=list(e.split(', '))
+        print(len(new_entry3))
+        # Checks if the number length is <4, if it is, tell the user
+        if len(new_entry3) != 10:
+            print("Incorrect attribute size")
+        else:
+            # Add pin entry to record database
+            #db.donor_add(new_entry2, c, conn)
+            db.see_donors(c)
+            #records(root,conn)
+        # Deletes the number array on an ENTER
+            del new_entry2[:]
+    if arg1 == "staff":
+        e = add_staff_entry.get()
+        print("Entry:",e)
+        new_entry4=list(e.split(', '))
+        print(len(new_entry4))
+        # Checks if the number length is <4, if it is, tell the user
+        if len(new_entry4) != 8:
+            print("Incorrect attribute size")
+        else:
+            # Add pin entry to record database
+            #db.donor_add(new_entry2, c, conn)
+            db.see_donors(c)
+            #records(root,conn)
+        # Deletes the number array on an ENTER
+            del new_entry2[:]
+    
 
 def buttonHandler_a(event, argument1):
     print(event)
@@ -101,7 +129,7 @@ blood_frame.place(relx=0.5, relwidth=1, relheight=1, anchor='n')
 # Data Entry Frame
 data_entry_frame = Frame(blood_frame, bg='light grey', bd=5)
 data_entry_frame.place(relx=0.5, rely=0.04, relwidth=0.75,
-                       relheight=0.25, anchor='n')
+                       relheight=0.3, anchor='n')
 
 '''
 # Data Display Frame and Label
@@ -165,7 +193,7 @@ m_font = tkinter.font.Font(family='Arial', size=12, weight=tkinter.font.BOLD)
 add_blood_drive_entry = Entry(data_entry_frame, width=40, font=("Times", 16))
 add_blood_drive_entry.grid(row=1, column=1, padx=20, pady=10)
 
-add_blood_drive_button = Button(data_entry_frame, text="Add Blood Drive")
+add_blood_drive_button = Button(data_entry_frame, text="Add Blood Drive",command=lambda arg1="blood_drive": buttonHandler("blood_drive"))
 add_blood_drive_button.bind("<Return>", lambda event,
                             arg1="blood_drive": buttonHandler_a(event, arg1))
 add_blood_drive_button.grid(row=1, column=2, sticky=W)
@@ -185,7 +213,7 @@ add_recipient_entry = Entry(data_entry_frame, width=40, font=("Times", 16))
 
 add_recipient_entry.grid(row=3, column=1, padx=20, pady=10)
 
-add_recipient_button = Button(data_entry_frame, text="Add Recipient")
+add_recipient_button = Button(data_entry_frame, text="Add Recipient",command=lambda arg1="recipient": buttonHandler("recipient"))
 add_recipient_button.bind("<Return>", lambda event,
                           arg1="recipient": buttonHandler_a(event, arg1))
 add_recipient_button.grid(row=3, column=2, sticky=W)
@@ -194,7 +222,7 @@ add_recipient_button.grid(row=3, column=2, sticky=W)
 add_staff_entry = Entry(data_entry_frame, width=40, font=("Times", 16))
 add_staff_entry.grid(row=4, column=1, padx=20, pady=10)
 
-add_staff_button = Button(data_entry_frame, text="Add Staff")
+add_staff_button = Button(data_entry_frame, text="Add Staff",command=lambda arg1="staff": buttonHandler("staff"))
 add_staff_button.bind("<Return>", lambda event,
                       arg1="staff": buttonHandler_a(event, arg1))
 add_staff_button.grid(row=4, column=2, sticky=W)
