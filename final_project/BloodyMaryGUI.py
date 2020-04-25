ff2b1afac630d16d41ccb94a0e090593ef0bd3d7
jtrapala <jane_trapala@my.uri.edu>
2020-04-24 22:41:33 -0400

    Cleaned up some more

--- final_project/BloodyMaryGUI.py
+++ final_project/BloodyMaryGUI.py
@@ -17,18 +17,11 @@ global conn, c
     database="BloodBank",
 )'''
 
+# For button images
 pth = os.getcwd()
-
-new_entry1 = Entry
-new_entry2 = Entry
-new_entry3 = Entry
-new_entry4 = Entry
-new_entry5 = Entry
-new_entry6 = Entry
 
 
 # Get connection going
-# conn = lite.connect('pin_dtb.db', timeout=120)
 conn = db.start_c1()
 
 # Get cursor
@@ -44,74 +37,29 @@ def destroy():
     root.destroy()
 
 
-def open_blood_window():
-    blood_window = Toplevel()
-    blood_window.mainloop()
-
-
-def open_bbanks_window():
-    bbank_window = Toplevel()
-    bbank_window.mainloop()
-
-
-def open_bdrives_window():
-    bdrive_window = Toplevel()
-    bdrive_window.mainloop()
-
-
-def open_donor_window():
-    donor_window = Toplevel()
-    donor_window.mainloop()
-
-
-def open_recps_window():
-    recipients_window = Toplevel()
-    recipients_window.mainloop()
-
-
-def open_staff_window():
-    staff_window = Toplevel()
-    staff_window.mainloop()
-
-
 def buttonHandler_table(arg1):
-
-    # Use enter function
     print("Using table: ", arg1)
-    # print number
     if arg1 == "blood_drive":
-        # e = blood_drive_entry.get()
-        # print("Entry:", e)
         sub_win = Toplevel(root)
         table_actions(sub_win, conn, arg1)
         sub_win.title("Blood Drive Window")
     elif arg1 == "blood":
-        # e = blood_drive_entry.get()
-        # print("Entry:", e)
         sub_win = Toplevel(root)
         table_actions(sub_win, conn, arg1)
-        sub_win.title("Blood Drive Window")
+        sub_win.title("Blood Inventory Window")
     elif arg1 == "b_bank":
-            # e = blood_drive_entry.get()
-            # print("Entry:", e)
         sub_win = Toplevel(root)
         table_actions(sub_win, conn, arg1)
         sub_win.title("Blood Bank Window")
     elif arg1 == "donor":
-        # e = donor_entry.get()
-        # print("Entry:", e)
         sub_win = Toplevel(root)
         table_actions(sub_win, conn, arg1)
         sub_win.title("Donor Window")
     elif arg1 == "recipient":
-        # e = recipient_entry.get()
-        # print("Entry:", e)
         sub_win = Toplevel(root)
         table_actions(sub_win, conn, arg1)
         sub_win.title("Recipient Window")
     elif arg1 == "staff":
-        # e = staff_entry.get()
-        # print("Entry:", e)
         sub_win = Toplevel(root)
         table_actions(sub_win, conn, arg1)
         sub_win.title("Staff Window")
@@ -120,13 +68,6 @@ def buttonHandler_table(arg1):
 def buttonHandler_a(event, argument1):
     print(event)
     buttonHandler_table(argument1)
-
-# Stores the entered username and hashed password in a dictionary in
-# another window, clears on submit
-
-
-def fake_command():
-    pass
 
 
 root = Tk()
@@ -183,8 +124,6 @@ blood_drive_button.bind("<Return>", lambda event,
 blood_drive_button.grid(row=3, column=2, sticky=S, padx=5)
 
 # Donor Section
-# donor_entry = Entry(blood_frame, width=40, font=bt_font)
-# donor_entry.grid(row=3, column=1, padx=20, pady=10)
 im3 = Image.open(pth + '\\button_imgs\\donor.jpg')
 im3 = im3.resize((100, 100))
 ph3 = ImageTk.PhotoImage(im3)
@@ -199,8 +138,6 @@ donor_button.bind("<Return>", lambda event,
 donor_button.grid(row=3, column=3, sticky=S, padx=5)
 
 # Recipient Section
-# recipient_entry = Entry(blood_frame, width=40, font=bt_font)
-# recipient_entry.grid(row=4, column=1, padx=20, pady=10)
 im4 = Image.open(pth + '\\button_imgs\\recp.jpg')
 im4 = im4.resize((100, 100))
 ph4 = ImageTk.PhotoImage(im4)
@@ -215,8 +152,6 @@ recipient_button.bind("<Return>", lambda event,
 recipient_button.grid(row=3, column=4, sticky=S, padx=5)
 
 # Staff Section
-# staff_entry = Entry(blood_frame, width=40, font=bt_font)
-# staff_entry.grid(row=5, column=1, padx=20, pady=10)
 im5 = Image.open(pth + '\\button_imgs\\staff.jpg')
 im5 = im5.resize((100, 100))
 ph5 = ImageTk.PhotoImage(im5)
@@ -228,9 +163,8 @@ staff_button.image = ph5
 staff_button.bind("<Return>", lambda event,
                   arg1="staff": buttonHandler_a(event, arg1))
 staff_button.grid(row=3, column=5, sticky=W, padx=5)
+
 # Blood Section
-# blood_inv_entry = Entry(blood_frame, width=40, font=bt_font)
-# blood_inv_entry.grid(row=6, column=1, padx=20, pady=10)
 im6 = Image.open(pth + '\\button_imgs\\blood_inv.jpg')
 im6 = im6.resize((100, 100))
 ph6 = ImageTk.PhotoImage(im6)
@@ -245,38 +179,9 @@ blood_inv_button.bind("<Return>", lambda event,
 blood_inv_button.grid(row=3, column=6, sticky=S, padx=5)
 
 
-# Menu
-blood_menu = Menu(root)
-root.config(menu=blood_menu)
-
-file_menu = Menu(blood_menu)
-blood_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_separator()
-file_menu.add_command(label="Exit", command=root.quit)
-
-tables_menu = Menu(blood_menu)
-blood_menu.add_cascade(label="Tables", menu=tables_menu)
-# work out this command
-tables_menu.add_command(label="Blood", command=open_blood_window)
-tables_menu.add_command(label="Blood Banks", command=open_bbanks_window)
-tables_menu.add_command(label="Blood Drives",
-                        command=open_bdrives_window)  # and this one
-tables_menu.add_command(
-    label="Donors", command=open_donor_window)  # oh and this one
-tables_menu.add_command(label="Recipients", command=open_recps_window)
-tables_menu.add_command(label="Staff", command=open_staff_window)
-
-# Status Bar
-# current_status = StringVar()
-# current_status.set("Waiting")
-
 my_status = Label(root, text="Running", bd=2,
                   relief="sunken", width=56, anchor=E)
 my_status.place(relx=0.5, rely=1, relwidth=1, anchor='s')
-
-# quitButton = tkinter.Button(root, text="QUIT", font=m_font,
-#                           bg = "orange red", command = destroy)
-# quitButton.grid(row=7, column=20)
 
 
 root.mainloop()
--- final_project/README.md
+++ final_project/README.md
@@ -1,1 +1,46 @@
-# CSC_436_project
+# Bloody Mary's Blood Bank
+
+Group Members:
+               Matt Pitman matt_pitman@my.uri.edu
+               Troy Durand tsdurand@my.uri.edu
+               Hassan Bhatti hassan_bhatti@my.uri.edu
+               Jane Trapala jane_trapala@my.uri.edu
+
+Repository link:
+    https://github.com/jtrapala/CSC_436_project
+
+## How to run
+
+### Linux
+
+### Windows
+
+## Screenshots
+
+### Main Window
+
+![alt text](screenshots/main.png "Main Window")
+
+### Blood Bank Window
+
+![alt text](screenshots/win1.png "Blood Bank Window")
+
+### Blood Drive Window
+
+![alt text](screenshots/win2.png "Blood Drive Window")
+
+### Donor Table Window
+
+![alt text](screenshots/win3.png "Main Window")
+
+## Recipient Table Window
+
+![alt text](screenshots/win4.png "Recipient Table Window")
+
+## Staff Table Window
+
+![alt text](screenshots/win5.png "Staff Table Window")
+
+## Blood Inventory Table Window
+
+![alt text](screenshots/win6.png "Blood Inventory Table Window")
